36f808d6116b0ffc114d4c6109e2c88f0bfcd356
candely desktop <candely1charifa@gmail.com>
2023-12-01 00:19:54 +0100

    Incompleted

---
+++ input.c
@@ -0,0 +1,116 @@
+#include "Maze.h"
+/**
+ * input - checks user input for movement
+ * @maze: 2D array defining maze map
+ * Return: void
+ */
+void input(int *maze)
+{
+    const uint8_t *keystate;
+    double oldTime;
+    double frameTime;
+    double oldDirX, oldPlaneX;
+    double moveSpeed;
+    double rotateSpeed;
+
+    keystate = SDL_GetKeyboardState(NULL);
+    oldTime = time;
+    time = SDL_GetTicks();
+    frameTime = (time - oldTime) / 1000.0;
+    moveSpeed = frameTime * 5.0;
+    rotateSpeed = frameTime * 3.0;
+
+    /* move forward if no wall in front */
+    if (keystate[SDL_SCANCODE_UP])
+    {
+        if (!*((int *)maze + (int)(pos.x + dir.x * moveSpeed)
+               * MAP_WIDTH + (int)pos.y))
+            pos.x += dir.x * moveSpeed;
+        if (!*((int *)maze + (int)pos.x * MAP_WIDTH +
+               (int)(pos.y + dir.y * moveSpeed)))
+            pos.y += dir.y * moveSpeed;
+    }
+    /* move backward if no wall behind */
+    if (keystate[SDL_SCANCODE_DOWN])
+    {
+        if (!*((int *)maze + (int)(pos.x - dir.x * moveSpeed) *
+               MAP_WIDTH + (int)(pos.y)))
+            pos.x -= dir.x * moveSpeed;
+        if (!*((int *)maze + (int)(pos.x) * MAP_WIDTH +
+               (int)(pos.y - dir.y * moveSpeed)))
+            pos.y -= dir.y * moveSpeed;
+    }
+    /* strafe left */
+    if (keystate[SDL_SCANCODE_LEFT])
+    {
+        if (!*((int *)maze + (int)(pos.x - plane.x * moveSpeed) *
+               MAP_WIDTH + (int)(pos.y)))
+            pos.x -= plane.x * moveSpeed;
+        if (!*((int *)maze + (int)(pos.x) * MAP_WIDTH +
+               (int)(pos.y - plane.y * moveSpeed)))
+            pos.y -= plane.y * moveSpeed;
+    }
+    /* strafe right */
+    if (keystate[SDL_SCANCODE_RIGHT])
+    {
+        if (!*((int *)maze + (int)(pos.x + plane.x * moveSpeed) *
+               MAP_WIDTH + (int)(pos.y)))
+            pos.x += plane.x * moveSpeed;
+        if (!*((int *)maze + (int)(pos.x) * MAP_WIDTH +
+               (int)(pos.y + plane.y * moveSpeed)))
+            pos.y += plane.y * moveSpeed;
+    }
+    /* rotate left */
+    if (keystate[SDL_SCANCODE_D])
+    {
+        oldDirX = dir.x;
+        dir.x = dir.x * cos(rotateSpeed) - dir.y * sin(rotateSpeed);
+        dir.y = oldDirX * sin(rotateSpeed) + dir.y * cos(rotateSpeed);
+
+        oldPlaneX = plane.x;
+        plane.x = plane.x * cos(rotateSpeed) - plane.y * sin(rotateSpeed);
+        plane.y = oldPlaneX * sin(rotateSpeed) + plane.y * cos(rotateSpeed);
+    }
+    /* rotate right */
+    if (keystate[SDL_SCANCODE_A])
+    {
+        oldDirX = dir.x;
+        dir.x = dir.x * cos(-rotateSpeed) - dir.y * sin(-rotateSpeed);
+        dir.y = oldDirX * sin(-rotateSpeed) + dir.y * cos(-rotateSpeed);
+
+        oldPlaneX = plane.x;
+        plane.x = plane.x * cos(-rotateSpeed) - plane.y * sin(-rotateSpeed);
+        plane.y = oldPlaneX * sin(-rotateSpeed) + plane.y * cos(-rotateSpeed);
+    }
+}
+
+/**
+ * quit - checks if user quits
+ * Return: True if user quits, else False
+ */
+bool quit(void)
+{
+    SDL_Event event;
+    bool quit;
+    uint32_t windowFlags;
+
+    quit = false;
+    while (SDL_PollEvent(&event) != 0)
+    {
+        if (event.type == SDL_QUIT)
+            quit = true;
+        if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
+            quit = true;
+        if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_f)
+        {
+            windowFlags = SDL_GetWindowFlags(window);
+
+            if (windowFlags & SDL_WINDOW_FULLSCREEN)
+                SDL_SetWindowFullscreen(window, SDL_FALSE);
+            else
+                SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+        }
+    }
+
+    return (quit);
+}
